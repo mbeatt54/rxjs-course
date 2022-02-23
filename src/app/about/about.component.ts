dfecd30f41cce1f630204455a9036bfd8d10fd66
Mark <mbeatt@hotmail.com>
2022-02-23 17:37:51 -0500

    intro to rxjs

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Observable, noop } from 'rxjs';
 
 @Component({
   selector: 'about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
+    const http$ = Observable.create((observer) => {
+      fetch('api/courses')
+        .then((response) => {
+          return response.json();
+        })
+        .then((body) => {
+          observer.next(body);
+          observer.complete();
+        })
+        .catch((err) => {
+          observer.errror(err);
+        });
+    });
 
+    http$.subscribe(
+      (courses) => console.log('courses: ', courses),
+      noop,
+      () => console.log('completed')
+    );
+  }
 }
